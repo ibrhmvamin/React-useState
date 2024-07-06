0aa7d1a9a05c2a504de9d83c1d4ec886fc7b8a76
ibrhmvamin <aminibrahimov52@gmail.com>
2024-07-06 12:01:47 +0400

    added styles

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import "./Form.css";
 
 export default function Form() {
   const [students, setStudents] = useState({});
+  const [notification, setNotification] = useState("");
 
   function handleNameChange(e) {
     setStudents({
@@ -38,44 +40,66 @@ export default function Form() {
     });
   }
 
-  function handleSave() {
-    alert("Student registered");
+  function handleSave(e) {
+    e.preventDefault();
+    setNotification("Student registered");
+    setTimeout(() => {
+      setNotification("");
+      setStudents({});
+    }, 2000);
   }
 
   return (
-    <div>
+    <div className="form-container">
       <form>
         <label>
-          Name : <input value={students.name} onChange={handleNameChange} />
+          Name :{" "}
+          <input value={students.name || ""} onChange={handleNameChange} />
         </label>
         <br />
         <label>
           Surname :{" "}
-          <input value={students.surname} onChange={handleSurnameChange} />
+          <input
+            value={students.surname || ""}
+            onChange={handleSurnameChange}
+          />
         </label>
         <br />
         <label>
           BirthDate :{" "}
-          <input value={students.birthDate} onChange={handleBirthDateChange} />
+          <input
+            type="date"
+            value={students.birthDate || ""}
+            onChange={handleBirthDateChange}
+          />
         </label>
         <br />
         <label>
-          Score : <input value={students.score} onChange={handleScoreChange} />
+          Score :{" "}
+          <input
+            type="number"
+            value={students.score || ""}
+            onChange={handleScoreChange}
+          />
         </label>
         <br />
         <label>
-          Image : <input value={students.image} onChange={handleImageChange} />
+          Image :{" "}
+          <input value={students.image || ""} onChange={handleImageChange} />
         </label>
         <br />
         <button onClick={handleSave}>Save</button>
       </form>
-      <section>
+      {notification && <p className="notification">{notification}</p>}
+      <section className="students-section">
         <h1>Students</h1>
-        <p>{students.name}</p>
-        <p>{students.surname}</p>
-        <p>{students.birthDate}</p>
-        <p>{students.score}</p>
-        <p>{students.image}</p>
+        <p>Name: {students.name}</p>
+        <p>Surname: {students.surname}</p>
+        <p>BirthDate: {students.birthDate}</p>
+        <p>Score: {students.score}</p>
+        {students.image && (
+          <img src={students.image} alt="Student" className="student-image" />
+        )}
       </section>
     </div>
   );
